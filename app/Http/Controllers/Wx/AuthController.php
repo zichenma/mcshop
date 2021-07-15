257c704c5bf217edccf93a297400bd37b54f069c
zichenma <zma@liaisionedu.com>
2021-07-15 00:56:13 -0400

    5-2 refactoring user sign up

--- app/Http/Controllers/Wx/AuthController.php
+++ app/Http/Controllers/Wx/AuthController.php
@@ -4,7 +4,11 @@
 namespace App\Http\Controllers\Wx;
 
 use App\Http\Controllers\Controller;
+use App\Services\UserServices;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -15,17 +19,58 @@ class AuthController extends Controller
         $password = $request -> input('password');
         $mobile = $request -> input('mobile');
         $code = $request -> input('code');
+        // 2. 验证参数是否为空
         if (empty($username) || empty($password) || empty($code) || empty($mobile)) {
             return [
                 'errno' => 401,
                 'errmsg' => '参数不对'
             ];
         }
-        // 2. 验证参数是否为空
+
+        $user = (new UserServices())->getByUserName($username);
+        if (!is_null($user)) {
+            return [
+                'errno' => 704,
+                'errmsg' => '用户名已注册'
+            ];
+        }
+        $validator = Validator::make(['mobile' => $mobile], ['mobile' => 'regex:/^1[0-9]{10}$']);
+        if ($validator->fails()) {
+            return [
+                'errno' => 707,
+                'errmsg' => '手机号格式不正确'
+            ];
+        }
+        $user = (new UserServices())->getByMobile($mobile);
+        if (!is_null($user)) {
+            return [
+                'errno' => 705,
+                'errmsg' => '手机号已注册'
+            ];
+        }
         // 3. 验证验证码是否正确
         // 4. 写入用户表
+        $user = new User();
+        // 相当于 user.setUsername(username)
+        $user->username = $username;
+        $user->password = Hash::make($password);
+        $user->avatar = "https://yanxuan.nosdn.127.net/80841d741d7fa3073e0ae27bf487339f.jpg?imageView&quality=90&thumbnail=64x64";
+        $user->nickname = $username;
+        $user->last_login_time = Carbon::now()->toDateString(); // 'Y-m-d H:i:s' 2020-05-17 16:17:34
+        $user->last_login_ip = $request->getClientIp();
+        $user->save();
         // 5. 新用户发券
         // 6. 返回用户信息和token
-
+        return [
+            'errno' => 0,
+            'errmsg' => '成功',
+            'data' => [
+                'token'=>'',
+                'userInfo'=> [
+                    'nickName' => $username,
+                    'avatarUrl' => $user->avatar
+                ]
+            ]
+        ];
     }
 }
--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -4,9 +4,26 @@
 namespace App\Services;
 
 
+use App\Models\User;
+
 class UserServices
 {
+    /**
+     * 根据用户名获取用户
+     * @param $username
+     * @return User|null|Model
+     */
     public function getByUserName($username) {
-        DB::table('users')->where('username', $username)->where('deleted', 0)->first();
+        return User::query()->where('username', $username)->where('deleted', 0)->first();
     }
+
+    /**
+     * 根据手机号获取用户
+     * @param $mobile
+     * @return User|null|Model
+     */
+    public function getByMobile($mobile) {
+        return User::query()->where('mobile', $mobile)->where('deleted', 0)->first();
+    }
+
 }
